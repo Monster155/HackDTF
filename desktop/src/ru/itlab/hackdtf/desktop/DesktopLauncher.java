a7261d4945aee15e74a63bf9f9c09343457e1bf2
Damir D <damir180501@mail.ru>
2019-11-16 12:57:57 +0300

    GameScreen, packages

--- desktop/src/ru/itlab/hackdtf/desktop/DesktopLauncher.java
+++ desktop/src/ru/itlab/hackdtf/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package ru.itlab.hackdtf.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.itlab.hackdtf.Main;
+import ru.itlab.hackdtf.Screens.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
