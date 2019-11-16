aa5626d0d7c524a689441562ab1be730e86fd169
Morozova Aleksandra <sashamorozova01@mail.ru>
2019-11-16 13:09:48 +0300

    r

--- core/src/ru/itlab/hackdtf/Bullet.java
+++ core/src/ru/itlab/hackdtf/Bullet.java
@@ -1,30 +1,41 @@
 package ru.itlab.hackdtf;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Bullet {
     int x;
     int y;
-    int widthScreen = Gdx.graphics.getWidth();
-    int heightScreen = Gdx.graphics.getHeight();
-    int width;
-    int height;
+    int width = Gdx.graphics.getWidth();
+    int height = Gdx.graphics.getHeight();
+    int speed;
     double angle;
+    Texture imgBullet;
 
-    Bullet() {
+    Bullet(int speed) {
         this.x = 0;
         this.y = 120;
-        this.width = 25;
-        this.height = 25;
-
+        this.speed = speed;
+        imgBullet = new Texture(Gdx.files.internal("strawberry(1).png"));
         //угол полета пули
         angle = Math.random() * 30;
     }
 
     //перемещение пули и её направление
-    public void update(int speed) {
+    public void update() {
         x += speed * Math.cos(angle);
         y += speed * Math.sin(angle);
+    }
+
+    //рисуем пулю
+    public void render(SpriteBatch batch) {
+        update();
+        batch.draw(imgBullet, x, y, 16, 16);
+    }
+
+    public void dispose(){
+        imgBullet.dispose();
     }
 
 }
