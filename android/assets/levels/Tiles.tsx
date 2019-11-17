b1dfe0df507729d2c112a0d34d94ba9989c85941
Damir D <damir180501@mail.ru>
2019-11-17 18:33:48 +0300

    Устаааал 2

--- android/assets/levels/Tiles.tsx
+++ android/assets/levels/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="Tiles" tilewidth="10" tileheight="10" tilecount="8640" columns="108">
- <image source="../TiledMap Projects/Tiles.png" width="1080" height="800"/>
+ <image source="Tiles.png" width="1080" height="800"/>
 </tileset>
